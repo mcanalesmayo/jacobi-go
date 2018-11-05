8cbc4b1ee56000040188abe8276034e8b0215016
Marcos Canales <mcanalesmayo@gmail.com>
2018-11-05 21:41:25 +0100

    simplified init parameters

--- jacobi_seq.go
+++ jacobi_seq.go
@@ -5,18 +5,15 @@ import (
 )
 
 func RunJacobi(initialValue float64, nDim int, maxIters int, tolerance float64) (matrix.Matrix, int, float64) {
-	var matA, matB matrix.Matrix
 	// The algorithm requires computing each grid cell as a 3x3 filter with no corners
 	// Therefore, we an aux matrix to keep the grid values in every iteration after computing new values
-	matA = matrix.NewMatrix(initialValue, nDim+2, matrix.Hot, matrix.Cold, matrix.Hot, matrix.Hot)
-	matB = matA.Clone(matrix.MatrixDef{
+	matA := matrix.NewMatrix(initialValue, nDim+2, matrix.Hot, matrix.Cold, matrix.Hot, matrix.Hot)
+	matB := matA.Clone(matrix.MatrixDef{
 		matrix.Coords{0, 0, nDim+1, nDim+1},
 		nDim+2,
 	})
 
-	matrixIters := nDim + 1
-
-	nIters, maxDiff := 0, 1.0
+	matrixIters, nIters, maxDiff := nDim + 1, 0, 1.0
 
 	for maxDiff > tolerance && nIters < maxIters {
 		maxDiff = 0.0

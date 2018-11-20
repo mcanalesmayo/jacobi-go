c3a6d6d9683207755a1b84b2b363ca6f849735f0
Marcos Canales <mcanalesmayo@gmail.com>
2018-11-20 22:54:09 +0100

    Info GOMAXPROCS

--- benchmark/benchmark_singlethread_test.go
+++ benchmark/benchmark_singlethread_test.go
@@ -3,6 +3,7 @@ package benchmark
 import (
 	"fmt"
 	"github.com/mcanalesmayo/jacobi-go"
+	"runtime"
 	"testing"
 )
 
@@ -25,6 +26,8 @@ func BenchmarkRunJacobi(b *testing.B) {
 		{0.5, 1024, 1000, 1.0e-4, 4},
 		{0.5, 4098, 1000, 1.0e-4, 4},
 	}
+
+	fmt.Printf("Running with GOMAXPROCS=%d\n", runtime.GOMAXPROCS(0))
 
 	for _, params := range experiments {
 		b.Run(fmt.Sprintf("%.4f,%d,%d,%.4f,%d", params.initialValue, params.nDim, params.maxIters, params.tolerance, params.nThreads), func(b *testing.B) {

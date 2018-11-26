d84ed6742ef6f33661dbbd27b7a31272363037ac
Marcos Canales <mcanalesmayo@gmail.com>
2018-11-26 22:50:33 +0100

    Add Stringable doc

--- utils/object_utils.go
+++ utils/object_utils.go
@@ -1,5 +1,7 @@
 package utils
 
+// Stringable represents a type that can be converted into a string
 type Stringable interface {
+	// ToString returns a string representation of the type
 	ToString() string
 }

2c1a06deb541014e62a681428a122f7b7dcab5f6
Marcos Canales <mcanalesmayo@gmail.com>
2018-12-06 13:31:04 +0100

    UPDATE clarification on epsilon usage for fp comparison

--- utils/float_utils.go
+++ utils/float_utils.go
@@ -11,8 +11,8 @@ const (
 )
 
 // CompareFloats compares two fp numbers taking into account that fp representation
-// results in approximations. Epsilon is taken as maximum tolerance for the difference
-// between both fp numbers.
+// results in approximations.
+// epsilon is taken as maximum absoluteValue(fpA-fpB).
 func CompareFloats(fpA, fpB float64, epsilon float64) bool {
 	res := false
 

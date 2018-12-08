95d1d7e551793428593c7d3dbe5dc907dbe683db
Marcos Canales <mcanalesmayo@gmail.com>
2018-12-08 12:08:23 +0100

    UPDATE benchmark, splitted multithreading and matrices

---
+++ benchmark/benchmark_matrices.go
@@ -0,0 +1,40 @@
+package benchmark
+
+import (
+	"fmt"
+	"github.com/mcanalesmayo/jacobi-go"
+	"github.com/mcanalesmayo/jacobi-go/model/matrix"
+	"testing"
+)
+
+type experiment struct {
+	matrixType   matrix.MatrixType
+	initialValue float64
+	nDim         int
+	maxIters     int
+	tolerance    float64
+	nThreads     int
+}
+
+// BenchmarkMatrixTypes runs the simulation with different matrix implementations to see the difference in terms of performance.
+// Current implementations:
+// - OneDimMatrixType, which allocates the complete matrix at once and accesses are performed with one index ([i*len+j]).
+// - TwoDimDividedMatrixType, which allocates the matrix row by row. This may lead the matrix to be divided in memory, causing performance degradation because of higher number of cache misses
+// - TwoDimContiguousMatrixType, which allocates the matrix at once (as OneDimMatrixType), but accesses are performed with two indexes ([i][j]).
+func BenchmarkMatrixTypes(b *testing.B) {
+	// Interleaving of multiple threads may favor the TwoDimDividedMatrixType matrix to be divided in memory, as one thread's matrix allocation may be interleaved with
+	// another thread's activity which requires memory allocation too
+	experiments := []experiment{
+		{matrix.TwoDimDividedMatrixType, 0.5, 2048, 1000, 1.0e-4, 4},
+		{matrix.TwoDimContiguousMatrixType, 0.5, 2048, 1000, 1.0e-4, 4},
+		{matrix.OneDimMatrixType, 0.5, 2048, 1000, 1.0e-4, 4},
+	}
+
+	for _, params := range experiments {
+		b.Run(fmt.Sprintf("%.4f,%d,%d,%.4f,%d,%s", params.initialValue, params.nDim, params.maxIters, params.tolerance, params.nThreads, params.matrixType.ToString()), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				jacobi.RunJacobi(params.initialValue, params.nDim, params.maxIters, params.tolerance, params.nThreads, params.matrixType)
+			}
+		})
+	}
+}
--- benchmark/benchmark_multithreading.go
+++ benchmark/benchmark_multithreading.go
@@ -17,29 +17,6 @@ type experiment struct {
 	nThreads     int
 }
 
-// BenchmarkMatrixTypes runs the simulation with different matrix implementations to see the difference in terms of performance.
-// Current implementations:
-// - OneDimMatrixType, which allocates the complete matrix at once and accesses are performed with one index ([i*len+j]).
-// - TwoDimDividedMatrixType, which allocates the matrix row by row. This may lead the matrix to be divided in memory, causing performance degradation because of higher number of cache misses
-// - TwoDimContiguousMatrixType, which allocates the matrix at once (as OneDimMatrixType), but accesses are performed with two indexes ([i][j]).
-func BenchmarkMatrixTypes(b *testing.B) {
-	// Interleaving of multiple threads may favor the TwoDimDividedMatrixType matrix to be divided in memory, as one thread's matrix allocation may be interleaved with
-	// another thread's activity which requires memory allocation too
-	experiments := []experiment{
-		{matrix.TwoDimDividedMatrixType, 0.5, 2048, 1000, 1.0e-4, 4},
-		{matrix.TwoDimContiguousMatrixType, 0.5, 2048, 1000, 1.0e-4, 4},
-		{matrix.OneDimMatrixType, 0.5, 2048, 1000, 1.0e-4, 4},
-	}
-
-	for _, params := range experiments {
-		b.Run(fmt.Sprintf("%.4f,%d,%d,%.4f,%d,%s", params.initialValue, params.nDim, params.maxIters, params.tolerance, params.nThreads, params.matrixType.ToString()), func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				jacobi.RunJacobi(params.initialValue, params.nDim, params.maxIters, params.tolerance, params.nThreads, params.matrixType)
-			}
-		})
-	}
-}
-
 func BenchmarkSingleVsMultithreading(b *testing.B) {
 	experiments := []experiment{
 		{matrix.TwoDimContiguousMatrixType, 0.5, 16, 1000, 1.0e-4, 1},
